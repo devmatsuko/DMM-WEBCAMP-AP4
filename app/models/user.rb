b066ab36840c4e303efc736936bf42e24000ad55
EC2 Default User <ec2-user@ip-172-31-66-161.ec2.internal>
2020-10-04 05:16:19 +0000

    [Add]フォロー機能のモデル、コントローラの追加。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,22 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  # フォローする側のリレーションシップ
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+  # N：Nのリレーションシップにはthroughを使う。user.following = user.followed.idとなるようにsourceを設定
+  has_many :following, through: :active_relationships, source: :followed
+
+  # フォロワーのリレーションシップ
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :followed_id
+  # N：Nのリレーションシップにはthroughを使う。user.followers = user.follower.idとなるようにsourceを設定
+  has_many :followers, through: :passive_relationships, source: :follower
+ 
+  #すでにフォロー済みであればture返す
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+  
   attachment :profile_image, destroy: false
 
   validates :name,length: {maximum: 20, minimum: 2}, uniqueness: true
